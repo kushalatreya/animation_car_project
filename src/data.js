a310bc2e5452febaadf8de4e81d1b27b422c1374
kushal atreya <kushalatreya@live.com>
2019-02-23 20:30:41 +0200

    added, sale bolean

--- src/data.js
+++ src/data.js
@@ -1,136 +1,169 @@
 export const CAR_LIST = [
-         {
-           id: 1,
-           title: "ASTON MARTIN VANTAGE V8 ROADSTER COUPE 2009",
-           img: "image/car-1.jpeg",
-           price: 100,
-           company: "ASTON MARTIN",
-           info:
-             "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-           inCart: false,
-           count: 0,
-           total: 0
-         },
-         {
-           id: 2,
-           title: "AUDI A1 ",
-           img: "image/car-2.jpeg",
-           price: 1600,
-           company: "AUDI",
-           info:
-             "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-           inCart: false,
-           count: 0,
-           total: 0
-         },
-         {
-           id: 3,
-           title: "AUDI A3",
-           img: "image/car-3.jpeg",
-           price: 80,
-           company: "AUDI",
-           info:
-             "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-           inCart: false,
-           count: 60,
-           total: 0
-         },
-         {
-           id: 4,
-           title: "Tesla Model S 85 Electric Sedan 2014",
-           img: "image/car-4.jpeg",
-           price: 168,
-           company: "TESLA",
-           info:
-             "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-           inCart: false,
-           count: 0,
-           total: 0
-         },
-         {
-           id: 5,
-           title:
-             "TOYOTA AURIS TOURING SPORTS PREMIUM HYBRID WAGON 2014",
-           img: "image/car-5.jpeg",
-           price: 114,
-           company: "TOYOTA",
-           info:
-             "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-           inCart: false,
-           count: 0,
-           total: 0
-         },
-         {
-           id: 6,
-             title: "TOYOTA COROLLA S SEDAN 2014",
-           img: "image/car-6.jpeg",
-           price: 170,
-             company: "TOYOTA ",
-           info:
-             "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-           inCart: false,
-           count: 0,
-           total: 0
-         },
-         {
-           id: 7,
-             title: "VOLKSWAGEN BEETLE CONVERTIBLE 2015",
-           img: "image/car-7.jpeg",
-           price: 130,
-             company: "VOLKSWAGEN",
-           info:
-             "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-           inCart: false,
-           count: 0,
-           total: 0
-         },
-         {
-           id: 8,
-             title: "VOLKSWAGEN CALIFORNIA COMFORTLINE MINIVAN 2014",
-           img: "image/car-8.jpeg",
-           price: 125,
-             company: "VOLKSWAGEN",
-           info:
-             "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-           inCart: false,
-           count: 0,
-           total: 0
-         },
-    {
-        id: 9,
-        title: "VOLKSWAGEN CALIFORNIA COMFORTLINE MINIVAN 2014",
-        img: "image/car-9.jpeg",
-        price: 125,
-        company: "VOLKSWAGEN",
-        info:
-            "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-        inCart: false,
-        count: 0,
-        total: 0
-    },
-    {
-        id: 10,
-        title: "BMW 1-SERIES 118D HATCHBACK 2014",
-        img: "image/car-10.jpeg",
-        price: 125,
-        company: "BMW",
-        info:
-            "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-        inCart: false,
-        count: 0,
-        total: 0
-    }
-       ];
-
-export const detailProduct = {
+  {
     id: 1,
-    title: "Google Pixel - Black",
+    title: "VINTAGE ROADSTER 1976",
     img: "image/car-1.jpeg",
-    price: 10,
-    company: "google",
+    price: 100,
+    company: "SKODA",
+    year:1976,
+    location:'Helsinki',
+    sale:false,
+    inCart: false,
     info:
-        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
     count: 0,
     total: 0
+  },
+  {
+    id: 2,
+    title: "AUDI A1 ",
+    img: "image/car-2.jpeg",
+    price: 160,
+    company: "AUDI",
+    year:2014,
+    location:'Oulu',
+    sale:false,
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "BMW A3",
+    img: "image/car-3.jpeg",
+    price: 80,
+    company: "BMW",
+    year: 2018,
+    location:'Helsinki',
+    sale:true,
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 60,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "MERCEDES BENZ Model S 2019",
+    img: "image/car-4.jpeg",
+    price: 170,
+    company: "MERCEDES BENZ",
+    year: 2019,
+    location:'Oulu',
+    sale:false,
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "BMW TOURING SPORTS PREMIUM HYBRID WAGON 2018",
+    img: "image/car-5.jpeg",
+    price: 110,
+    company: "BMW",
+    year: 2018,
+    location:'Helsinki',
+    sale:false,
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "MERCEDES S SUV 2018",
+    img: "image/car-6.jpeg",
+    price: 170,
+    company: "MERCEDES ",
+    year: 2018,
+    location:'Helsinki',
+    sale:false,
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "MERCEDES W SERIES 2015",
+    img: "image/car-7.jpeg",
+    price: 130,
+    company: "MERCEDES",
+    year:2015,
+    location:'Oulu',
+    sale:false,
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "TOYOTA CALIFORNIA COMFORTLINE MINITRUCK 2014",
+    img: "image/car-8.jpeg",
+    price: 125,
+    company: "TOYOTA",
+    year:2014,
+    location:'Helsinki',
+    sale:false,
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 9,
+    title: "MERCEDES E CLASS 2014",
+    img: "image/car-9.jpeg",
+    price: 125,
+    company: "MERCEDES",
+    year:2014,
+    location:'Helsinki',
+    sale:false,
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 10,
+    title: "BMW 1-SERIES 118D HATCHBACK 2014",
+    img: "image/car-10.jpeg",
+    price: 125,
+    company: "BMW",
+    year:2014,
+    location:'Helsinki',
+    sale:false,
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "VINTAGE ROADSTER 1976",
+  img: "image/car-1.jpeg",
+  price: 100,
+  company: "SKODA",
+  year:1976,
+  location:'Helsinki',
+  sale:false,
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
 };
