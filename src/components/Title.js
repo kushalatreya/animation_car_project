43e8e4e30ad28cb087a62f8293fde93a3e42eee5
kushal atreya <kushalatreya@live.com>
2019-02-23 20:30:06 +0200

    title with props as headline text

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ export default function Title({name,title}) {
   return (
     <React.Fragment>
           <div className="row">
-              <div className="col-10 mx-auto my-2 text-center text-title">
+              <div className="col-10 mx-auto my-0 text-center text-title">
                   <h1 className="text-capitalize font-weight-bold">
                       {name}
                       <span> <strong className="text-blue">{title}</strong></span>{/* css is assigned in App.css, text-blue */}
