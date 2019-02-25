54835adaa4c7c2170f8c97c453138b7bc737e772
kushal atreya <kushalatreya@live.com>
2019-02-25 11:12:48 +0200

    search bar is changed

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -4,12 +4,12 @@ import { HeadWrapper } from "../../components/StyledComponent";
 export default function Header() {
   return (
     <React.Fragment>
-          <div class="card bg-dark h-25">
-              <img class=" mh-40 img-fluid d-block mw-100 " src="https://www.gkluk.com/wp-content/uploads/2015/09/123.jpeg" alt="Card image"/>
-                  <div class="card-img-overlay text-white">
-                      <h5 class="card-title">Card title</h5>
-                      <h2 class="card-text">Leasing of Luxiry Car, Never Been Easier.</h2>
-                      <p class="card-text">Last updated 3 mins ago</p>
+          <div className="card bg-dark">
+              <img className=" mh-40 img-fluid d-block mw-100 " src="https://www.gkluk.com/wp-content/uploads/2015/09/123.jpeg" alt="hero_image"/>
+                  <div className="card-img-overlay text-white">
+                  <h5 className="card-title">Autonvuokraus Oulu,</h5>
+                      <h2 className="card-text">Renting Luxiry Car, Never Been Easier.</h2>
+                  <p className="card-text">--Voted best service company 2018 <i class="fas fa-award"></i> </p>
                   </div>
 </div>
           {/* d-block w-100 img-fluid */}
@@ -21,18 +21,24 @@ export default function Header() {
                 <div className="col-lg-12">
                   <div className="row">
                     <div className="col-lg-3 col-md-3 col-sm-12 p-0">
-                      <input
-                        type="text"
-                        className="form-control search-slt"
-                        placeholder="Enter Pickup City"
-                      />
-                    </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                                        
                       <input
                         type="text"
                         className="form-control search-slt"
                         placeholder="Enter Drop City"
                       />
+                    </div>
+                    <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                                          
+                       <select
+                        className="form-control search-slt"
+                        id="exampleFormControlSelect1"
+                      >
+                        <option>Select Location</option>
+                        <option>Helsinki</option>
+                        <option>Oulu</option>
+                        
+                      </select>
                     </div>
                     <div className="col-lg-3 col-md-3 col-sm-12 p-0">
                       <select
